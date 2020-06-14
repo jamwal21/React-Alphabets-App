2644cb68d2a219b6b1f839420b76cc84306535e5
Abhinav Jamwal <abhinavjamwal302@gmail.com>
2020-06-14 11:16:13 +0530

    Updates

---
+++ precache-manifest.8df761daee6b455790bafc5f0775af0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5839ac720e39aaa59df4c63ea119238",
+    "url": "/React-Alphabets-App/index.html"
+  },
+  {
+    "revision": "b6a790cec08155d1be84",
+    "url": "/React-Alphabets-App/static/css/main.e48a8ee7.chunk.css"
+  },
+  {
+    "revision": "7df5fe97b53eab6061c2",
+    "url": "/React-Alphabets-App/static/js/2.c6a4199d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Alphabets-App/static/js/2.c6a4199d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a790cec08155d1be84",
+    "url": "/React-Alphabets-App/static/js/main.e539e7bf.chunk.js"
+  },
+  {
+    "revision": "822936bdf7e2b823e9ce",
+    "url": "/React-Alphabets-App/static/js/runtime-main.26a5afcf.js"
+  }
+]);
